59cbf41d206fa7a1ce4cf054fc8b9081712ca49b
james a <jamesacoding@gmail.com>
2022-01-14 11:09:51 -0500

    coding practice

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-//PERSON CONSTRUCTORS
+class Person {
+  constructor(firstName, lastName, dob){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
 
+  greeting() {
+    return `hello there ${this.firstName} ${this.lastName}`
+  }
+
+  calculateAge(){
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  getsMarried(newLastName) {
+    this.lastName = newLastName
+  }
+}
+
+const mary = new Person('Mary', 'Williams', '11-13-1980');
+
+console.log(mary)
